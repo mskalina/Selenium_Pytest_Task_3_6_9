844adf9cefdc18c8cb07f79ddedcdf145ef36aa4
mskalina <89806037+mskalina@users.noreply.github.com>
2022-03-31 13:03:53 +0300

    changes due to task criteria

--- conftest.py
+++ conftest.py
@@ -1,6 +1,21 @@
 import pytest
-#from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture
+def browser(request):
+    browser = None
+    user_language = request.config.getoption("language")
+    print("\nstart chrome browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    #---Нужно отредактировать или удалить эту строку для провекри теста, к сожалению, без этого аргумента Хром не запускается, хоть в PATH все добавлено
+    options.add_argument(r"C:\Users\wwwma\environments\selenium_course\chromedriver") 
+    #---
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en",
--- test_items.py
+++ test_items.py
@@ -1,25 +1,8 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import time
 
 link=" http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 #link="http://google.com/bla"
-
-@pytest.fixture
-def browser(request):
-    browser = None
-    user_language = request.config.getoption("language")
-    print("\nstart chrome browser for test..")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    #---Нужно отредактировать или удалить эту строку для провекри теста, к сожалению, без этого аргумента Хром не запускается, хоть в PATH все добавлено
-    options.add_argument(r"C:\Users\wwwma\environments\selenium_course\chromedriver") 
-    #---
-    browser = webdriver.Chrome(options=options)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
     
 def test_add_button_is_present(browser):
     browser.get(link) 
